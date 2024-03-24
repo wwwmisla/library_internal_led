4fba32cfbd17f513d2943c8cc15db84ed0317ac0
Misla Wislaine <mislawislaine12@gmail.com>
2024-03-24 16:14:34 -0300

    :sparkles: feat: Examples
    Aperfeiçoamento do InternalLed.ino

--- Examples/InternalLed/InternalLed.ino
+++ Examples/InternalLed/InternalLed.ino
@@ -1,26 +1,36 @@
 /*
     Internal LED (LED Interno)
 
+    Biblioteca para controlar um LED interno.
+
     InternalLed.ino - Exemplo
 
-    Um exemplo de como criar um LED vermelho que irá acender e após um determinado tempo irá apagar.
+    Um exemplo de como criar um LED que irá acender e após um determinado tempo irá apagar.
 
     O circuito:
-    * InternalLed conectado ao pino D2
+    * InternalLed conectado ao pino D13 do Arduino.
 
-    Criado em 20/03/2024
+    Criado em 20/03/2024 | Atualizado em 23/03/2024
     Por Um Robô por Aluno
 
     Copyright (c) 2024 Um Robô por Aluno. Todos os direitos reservados.
 */
 
+#include <InternalLed.h>
+
+// Define o pino ao qual o LED está conectado
+#define LED_PIN 13
+
 // Mapeamento de pinos | Criação de objetos
-InternalLed led(13)   ///< O pino conectado ao led interno
+InternalLed led(LED_PIN)   ///< O pino conectado ao led interno
 
 /**
  * A função setup do Arduino usada para configuração e configurar tarefas.
  */
-void setup() {}
+void setup() {
+  // Inicializa o objeto InternalLed
+  led.begin();
+}
 // Fecha a função setup()
 
 /**
@@ -29,13 +39,23 @@ void setup() {}
  */
 void loop() {
 
-  led.acendeLed();  // Chamando a função para ligar o LED
+  // Acende o LED (pino D13)
+  led.acendeLed();
 
-  setDelay(1000);   // Aguarda 1 segundo (1000 milissegundos)
+  /**
+   * Aguarda um intervalo de tempo em milissegundos antes de prosseguir para a próxima função
+   * delay(milissegundos);
+   */
+  delay(1000);
 
-  led.apagaLed();   // Chamando a função para desligar o LED
+  // Apaga o LED (pino D13)
+  led.apagaLed();
 
-  setDelay(500);    // Aguarda 0,5 segundos (500 milissegundos)
+  /**
+   * Aguarda um intervalo de tempo em milissegundos antes de prosseguir para a próxima função
+   * delay(milissegundos);
+   */
+  delay(500);
 
 }
 // Fecha a função void loop()
